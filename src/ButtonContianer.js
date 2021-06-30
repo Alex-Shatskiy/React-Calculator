e73bae85a2dfe3b3d11ab827994048fb531099cd
Alex-Shatskiy <alexlev706@gmail.com>
2021-06-30 22:48:17 +1200

    material ui styling

--- src/ButtonContianer.js
+++ src/ButtonContianer.js
@@ -1,48 +1,131 @@
-import React from "react";
+import React from "react"
+import { Grid, ButtonGroup, Button } from "@material-ui/core"
+import { withStyles } from "@material-ui/core/styles"
+
+const styles = (theme) => ({
+  buttonGroup: {
+    width: "100%",
+    height: "18%",
+    justifyContent: "center",
+  },
+  button: {
+    width: "25%",
+    height: "18vh",
+  },
+})
+
 class ButtonContainer extends React.Component {
   render() {
-    const {
-      changeState,
-      clear,
-      changeOperator,
-      calculate,
-      backSpace
-    } = this.props;
+    const { changeState, clear, changeOperator, calculate, backSpace } =
+      this.props
+    const { classes } = this.props
     return (
       <>
-        <div className="buttonContainer">
-          <div>
-            <button onClick={() => clear()}>C</button>
-            <button onClick={() => changeOperator("/")}>/</button>
-            <button onClick={() => changeOperator("*")}>*</button>
-            <button onClick={() => backSpace()}>{"<-"} </button>
-          </div>
-          <div>
-            <button onClick={() => changeState(7)}>7</button>
-            <button onClick={() => changeState(8)}>8</button>
-            <button onClick={() => changeState(9)}>9</button>
-            <button onClick={() => changeOperator("-")}>-</button>
-          </div>
-          <div>
-            <button onClick={() => changeState(4)}>4</button>
-            <button onClick={() => changeState(5)}>5</button>
-            <button onClick={() => changeState(6)}>6</button>
-            <button onClick={() => changeOperator("+")}>+</button>
-          </div>
-          <div>
-            <button onClick={() => changeState(1)}>1</button>
-            <button onClick={() => changeState(2)}>2</button>
-            <button onClick={() => changeState(3)}>3</button>
-            <button onClick={() => changeState(".")}>.</button>
-          </div>
-          <div>
-            <button>0</button>
-            <button onClick={() => calculate()}>=</button>
-          </div>
-        </div>
+        <ButtonGroup
+          size="large"
+          color="primary"
+          aria-label="large outlined primary button group"
+          fullWidth="true"
+          className={classes.buttonGroup}
+        >
+          <Button className={classes.button} onClick={() => clear()}>
+            C
+          </Button>
+          <Button
+            className={classes.button}
+            onClick={() => changeOperator("/")}
+          >
+            /
+          </Button>
+          <Button
+            className={classes.button}
+            onClick={() => changeOperator("*")}
+          >
+            *
+          </Button>
+          <Button className={classes.button} onClick={() => backSpace()}>
+            {"<-"}{" "}
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup
+          size="large"
+          color="primary"
+          aria-label="large outlined primary button group"
+          className={classes.buttonGroup}
+        >
+          <Button className={classes.button} onClick={() => changeState(7)}>
+            7
+          </Button>
+          <Button className={classes.button} onClick={() => changeState(8)}>
+            8
+          </Button>
+          <Button className={classes.button} onClick={() => changeState(9)}>
+            9
+          </Button>
+          <Button
+            className={classes.button}
+            onClick={() => changeOperator("-")}
+          >
+            -
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup
+          size="large"
+          color="primary"
+          aria-label="large outlined primary button group"
+          className={classes.buttonGroup}
+        >
+          <Button className={classes.button} onClick={() => changeState(4)}>
+            4
+          </Button>
+          <Button className={classes.button} onClick={() => changeState(5)}>
+            5
+          </Button>
+          <Button className={classes.button} onClick={() => changeState(6)}>
+            6
+          </Button>
+          <Button
+            className={classes.button}
+            onClick={() => changeOperator("+")}
+          >
+            +
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup
+          size="large"
+          color="primary"
+          aria-label="large outlined primary button group"
+          className={classes.buttonGroup}
+        >
+          <Button className={classes.button} onClick={() => changeState(1)}>
+            1
+          </Button>
+          <Button className={classes.button} onClick={() => changeState(2)}>
+            2
+          </Button>
+          <Button className={classes.button} onClick={() => changeState(3)}>
+            3
+          </Button>
+          <Button className={classes.button} onClick={() => changeState(".")}>
+            .
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup
+          size="large"
+          color="primary"
+          aria-label="large outlined primary button group"
+          className={classes.buttonGroup}
+        >
+          <Button className={classes.button} onClick={() => changeState(0)}>
+            0
+          </Button>
+          <Button className={classes.button} onClick={() => calculate()}>
+            =
+          </Button>
+        </ButtonGroup>
       </>
-    );
+    )
   }
 }
 
-export default ButtonContainer;
+export default withStyles(styles, { withTheme: true })(ButtonContainer)
--- src/NumberTextBox.js
+++ src/NumberTextBox.js
@@ -1,21 +1,40 @@
-import React from "react";
+import React from "react"
+import { Grid } from "@material-ui/core"
 class NumberTextBox extends React.Component {
   render() {
-    let { value1, value2, operator, total } = this.props;
-    console.log(this.props);
+    let { value1, value2, operator, total } = this.props
+    console.log(this.props)
     return (
       <>
-        <div className="numberBox">
-          <lable className="displayOutput">
-            {value1}
-            {operator}
-            {value2}
-          </lable>
-          <lable className="input">{total}</lable>
-        </div>
+        <Grid
+          container
+          spacing={3}
+          style={{ width: "100vw", justifyContent: "center" }}
+        >
+          <Grid
+            item
+            xs={12}
+            style={{ display: "inherit", justifyContent: " center" }}
+          >
+            <lable className="displayOutput" style={{ fontSize: "30px" }}>
+              {value1 == "" && total == "" ? (
+                <>Calculator</>
+              ) : (
+                <>
+                  {value1}
+                  {operator}
+                  {value2}
+                </>
+              )}
+            </lable>
+            <lable className="input" style={{ fontSize: "30px" }}>
+              {total}
+            </lable>
+          </Grid>
+        </Grid>
       </>
-    );
+    )
   }
 }
 
-export default NumberTextBox;
+export default NumberTextBox
